6d04ffdcbcb5a9e6f02c08873055c87cca7580ce
Gregory Gonta <gregory.gonta@tacanslabs.com>
2022-11-02 16:27:27 +0200

    return F32

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,12 @@ extern "C" {
 
 #[derive(Copy, Clone, Debug)]
 #[repr(C)]
+pub struct float32_t {
+    pub v: u32,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
 pub struct float64_t {
     pub v: u64,
 }
@@ -41,12 +47,42 @@ pub const softfloat_flag_invalid: u8 = 16;
 extern "C" {
     pub fn softfloat_raiseFlags(_: uint_fast8_t);
 
+    pub fn ui32_to_f32(_: u32) -> float32_t;
     pub fn ui32_to_f64(_: u32) -> float64_t;
+    pub fn ui64_to_f32(_: u64) -> float32_t;
     pub fn ui64_to_f64(_: u64) -> float64_t;
 
+    pub fn i32_to_f32(_: i32) -> float32_t;
     pub fn i32_to_f64(_: i32) -> float64_t;
 
+    pub fn i64_to_f32(_: i64) -> float32_t;
     pub fn i64_to_f64(_: i64) -> float64_t;
+
+    pub fn f32_to_ui32(_: float32_t, _: uint_fast8_t, _: bool) -> uint_fast32_t;
+    pub fn f32_to_ui64(_: float32_t, _: uint_fast8_t, _: bool) -> uint_fast64_t;
+    pub fn f32_to_i32(_: float32_t, _: uint_fast8_t, _: bool) -> int_fast32_t;
+    pub fn f32_to_i64(_: float32_t, _: uint_fast8_t, _: bool) -> int_fast64_t;
+    pub fn f32_to_ui32_r_minMag(_: float32_t, _: bool) -> uint_fast32_t;
+    pub fn f32_to_ui64_r_minMag(_: float32_t, _: bool) -> uint_fast64_t;
+    pub fn f32_to_i32_r_minMag(_: float32_t, _: bool) -> int_fast32_t;
+    pub fn f32_to_i64_r_minMag(_: float32_t, _: bool) -> int_fast64_t;
+    pub fn f32_to_f64(_: float32_t) -> float64_t;
+
+    pub fn f32_roundToInt(_: float32_t, _: uint_fast8_t, _: bool) -> float32_t;
+    pub fn f32_add(_: float32_t, _: float32_t) -> float32_t;
+    pub fn f32_sub(_: float32_t, _: float32_t) -> float32_t;
+    pub fn f32_mul(_: float32_t, _: float32_t) -> float32_t;
+    pub fn f32_mulAdd(_: float32_t, _: float32_t, _: float32_t) -> float32_t;
+    pub fn f32_div(_: float32_t, _: float32_t) -> float32_t;
+    pub fn f32_rem(_: float32_t, _: float32_t) -> float32_t;
+    pub fn f32_sqrt(_: float32_t) -> float32_t;
+    pub fn f32_eq(_: float32_t, _: float32_t) -> bool;
+    pub fn f32_le(_: float32_t, _: float32_t) -> bool;
+    pub fn f32_lt(_: float32_t, _: float32_t) -> bool;
+    pub fn f32_eq_signaling(_: float32_t, _: float32_t) -> bool;
+    pub fn f32_le_quiet(_: float32_t, _: float32_t) -> bool;
+    pub fn f32_lt_quiet(_: float32_t, _: float32_t) -> bool;
+    pub fn f32_isSignalingNaN(_: float32_t) -> bool;
 
     pub fn f64_to_ui32(_: float64_t, _: uint_fast8_t, _: bool) -> uint_fast32_t;
     pub fn f64_to_ui64(_: float64_t, _: uint_fast8_t, _: bool) -> uint_fast64_t;
@@ -56,6 +92,7 @@ extern "C" {
     pub fn f64_to_ui64_r_minMag(_: float64_t, _: bool) -> uint_fast64_t;
     pub fn f64_to_i32_r_minMag(_: float64_t, _: bool) -> int_fast32_t;
     pub fn f64_to_i64_r_minMag(_: float64_t, _: bool) -> int_fast64_t;
+    pub fn f64_to_f32(_: float64_t) -> float32_t;
 
     pub fn f64_roundToInt(_: float64_t, _: uint_fast8_t, _: bool) -> float64_t;
     pub fn f64_add(_: float64_t, _: float64_t) -> float64_t;
@@ -93,16 +130,47 @@ mod tests {
             softfloat_exceptionFlags_read_helper,
             softfloat_exceptionFlags_write_helper,
             softfloat_raiseFlags,
+            ui32_to_f32,
             ui32_to_f64,
+            ui64_to_f32,
             ui64_to_f64,
+            i32_to_f32,
             i32_to_f64,
+            i64_to_f32,
             i64_to_f64,
+            f32_to_ui32,
+            f32_to_ui64,
+            f32_to_i32,
+            f32_to_i64,
+            f32_to_ui32_r_minMag,
+            f32_to_ui64_r_minMag,
+            f32_to_i32_r_minMag,
+            f32_to_i64_r_minMag,
+            f32_to_f64,
+            f32_roundToInt,
+            f32_add,
+            f32_sub,
+            f32_mul,
+            f32_mulAdd,
+            f32_div,
+            f32_rem,
+            f32_sqrt,
+            f32_eq,
+            f32_le,
+            f32_lt,
+            f32_eq_signaling,
+            f32_le_quiet,
+            f32_lt_quiet,
+            f32_isSignalingNaN,
+            f64_to_ui32,
             f64_to_ui64,
+            f64_to_i32,
             f64_to_i64,
             f64_to_ui32_r_minMag,
             f64_to_ui64_r_minMag,
             f64_to_i32_r_minMag,
             f64_to_i64_r_minMag,
+            f64_to_f32,
             f64_roundToInt,
             f64_add,
             f64_sub,
